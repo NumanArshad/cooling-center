8fc8c813bc30fb629dfe48b1bd581a4a393952fa
Numan Arshad <afshanarsha2783@gmail.com>
2023-10-25 01:13:25 +0500

    theme scripts, assets and js files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head >
+      <meta charSet="utf-8" />
+      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+      <meta content="" name="keywords" />
+      <meta content="" name="description" />
+      <title>hello boy</title>
+      <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="lib/wow/wow.min.js"></script>
+      <script src="lib/easing/easing.min.js"></script>
+      <script src="lib/waypoints/waypoints.min.js"></script>
+      <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+      <script src="lib/counterup/counterup.min.js"></script>
+      <script src="lib/parallax/parallax.min.js"></script>
+
+      <script src="js/main.js"></script>
+    </Head>
+    <Component {...pageProps} />
+
+  </>
+
 }
